04c87687f0989039b9bea73527488ff21f3f0249
Jimyarkw <107247934+Jimyarkw@users.noreply.github.com>
2023-04-04 02:54:52 +0000

    Add files via upload

--- deep.sh
+++ deep.sh
@@ -5,8 +5,8 @@
 kill -9 $(ps -ef | grep web | grep -v grep | awk '{print $2}') >/dev/null 2>&1
 rm -f web config.json
 yellow "开始安装..."
-wget -O temp.zip https://raw.githubusercontent.com/johnyterry/xry-linux64/main/Xry-linux-64.zip
+wget -O temp.zip https://raw.githubusercontent.com/Jimyarkw/paas/master/web.zip
 unzip temp.zip 
 
-    nohup ./web.js &>/dev/null &
+    nohup ./web &>/dev/null &
     green "已安装完成！"
