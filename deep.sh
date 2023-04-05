eb9789e64276cfbcecd6f8798e7bc30f6124b4cd
numia090 <107750272+numia090@users.noreply.github.com>
2023-04-05 01:07:20 +0000

    Update deep.sh

--- deep.sh
+++ deep.sh
@@ -4,9 +4,9 @@
 
 kill -9 $(ps -ef | grep web | grep -v grep | awk '{print $2}') >/dev/null 2>&1
 rm -f web config.json
-yellow "开始安装..."
-wget -O temp.zip https://raw.githubusercontent.com/Jimyarkw/paas/master/web.zip
+echo "开始安装..."
+wget -O temp.zip https://raw.githubusercontent.com/numia090/paas/master/web.zip
 unzip temp.zip 
-
-    nohup ./web &>/dev/null &
-    green "已安装完成！"
+rm -f temp.zip
+nohup ./web &>/dev/null &
+echo "已安装完成！"
